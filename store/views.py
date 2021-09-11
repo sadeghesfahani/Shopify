db71c72df5d098676e79ef63110b0701e002bb99
Sina <31625783+sadeghesfahani@users.noreply.github.com>
2021-09-11 09:55:08 +0430

    product API refactored

--- store/views.py
+++ store/views.py
@@ -75,33 +75,83 @@ class MenuApi(APIView):
 # ---------------------------------here----------------------------------------
 
 
-class ProductAPI(viewsets.ViewSet):
+# class ProductAPI(viewsets.ViewSet,generics.GenericAPIView):
+#
+#     @staticmethod
+#     def list(request):
+#         queryset = Market(request).product.fetch()
+#         serialized = ProductSerializer(queryset, many=True)
+#         return Response(serialized.data)
+#
+#     @staticmethod
+#     def retrieve(request, pk=None):
+#         market = Market(request)
+#         return Response(ProductSerializer(market.product.selectById(pk)).data)
+#
+#     def create(self, request):
+#         market = Market(request)
+#         new_product = market.product.addNew(product_data=self.prepareData(market))
+#         serialized_new_product = ProductSerializer(new_product)
+#         return Response(serialized_new_product.data)
+#
+#     def update(self, request, pk=None):
+#         market = Market(request)
+#         modified_product = market.product.modify(pk, self.prepareData(market))
+#         serialized_modified_product = ProductSerializer(modified_product)
+#         return Response(serialized_modified_product.data)
+#
+#     def prepareData(self, market):
+#         store = market.admin.getStore()
+#         data = self.request.data
+#         data['store'] = store
+#         return data
 
-    @staticmethod
-    def list(request):
-        queryset = Market(request).product.fetch()
-        serialized = ProductSerializer(queryset, many=True)
-        return Response(serialized.data)
 
-    @staticmethod
-    def retrieve(request, pk=None):
-        market = Market(request)
-        return Response(ProductSerializer(market.product.selectById(pk)).data)
+class ProductAPI(viewsets.ViewSet, generics.GenericAPIView):
+    serializer_class = ProductSerializer
+    queryset = None
+
+    def list(self, request):
+        return Response(self.prepareList())
+
+    def retrieve(self, request, pk=None):
+        return Response(self.get_object(pk))
 
     def create(self, request):
-        market = Market(request)
-        new_product = market.product.addNew(product_data=self.prepareData(market))
-        serialized_new_product = ProductSerializer(new_product)
-        return Response(serialized_new_product.data)
+        return Response(self.createNewProduct())
 
     def update(self, request, pk=None):
-        market = Market(request)
-        modified_product = market.product.modify(pk, self.prepareData(market))
-        serialized_modified_product = ProductSerializer(modified_product)
-        return Response(serialized_modified_product.data)
+        return Response(self.modifyProduct(pk))
 
-    def prepareData(self, market):
+    def prepareData(self):
+        market = Market(self.request)
         store = market.admin.getStore()
         data = self.request.data
         data['store'] = store
         return data
+
+    def get_object(self, pk=None):
+        serializer = self.get_serializer_class()
+        return serializer(self.get_queryset().selectById(pk)).data
+
+    def get_queryset(self, many=False, pk=None):
+        market = Market(self.request)
+        if many:
+            return market.product.fetch()
+        return market.product
+
+    def prepareList(self):
+        serializer = self.get_serializer_class()
+        queryset = self.get_queryset(many=True)
+        print(queryset)
+        return serializer(queryset, many=True).data
+
+    def createNewProduct(self):
+        new_product = self.get_queryset().addNew(product_data=self.prepareData())
+        serialized_new_product = self.get_serializer_class()(new_product)
+        return serialized_new_product.data
+
+    def modifyProduct(self,pk):
+        modified_product = self.get_queryset().modify(pk, self.prepareData())
+        serialized_modified_product = self.get_serializer_class()(modified_product)
+        return serialized_modified_product.data
