dc628118bc6ceb3f02d5580cc5b58657aea46fed
Sina <31625783+sadeghesfahani@users.noreply.github.com>
2021-10-01 12:20:13 +0330

    address and user interface foundation laid

---
+++ frontend/shopify/src/components/Address.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Address extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Address;
---
+++ frontend/shopify/src/components/User.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class User extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default User;
