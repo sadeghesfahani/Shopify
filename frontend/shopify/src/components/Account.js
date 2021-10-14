f10448a735c10f29a750d80955ad385f68c7ed8c
sina <sadeghesfahani.sina@gmail.com>
2021-10-14 07:53:13 +0330

    froms put into account

--- frontend/shopify/src/components/Account.js
+++ frontend/shopify/src/components/Account.js
@@ -1,10 +1,43 @@
 import React, {Component} from 'react';
+import Address from "./Address";
 
 class Account extends Component {
-    getOrderHistory=(howmuch=false)=>{
-        if (howmuch){
+    state = {
+        address: [],
+        first_name:"",
+        last_name:""
+    }
+    getOrderHistory = (howmuch = false) => {
+        if (howmuch) {
             const url = ""
         }
+    }
+    handleInputAddress = ({currentTarget: input}) => {
+        this.setState({address: input.value})
+    }
+    updateUserForm = (data) => {
+        if (this.state.first_name !== data.first_name && this.state.last_name !== data.last_name){
+            this.setState({first_name: data.first_name, last_name: data.last_name})
+        }
+
+    }
+    updateAddress =(data)=>{
+        this.setState({address:data})
+    }
+    getInfo = () => {
+        const option = {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json, text/plain',
+                'Content-Type': 'application/json;charset=UTF-8',
+                'Authorization': `token ${this.props.user ? this.props.user : localStorage.getItem('user')}`
+            }
+        }
+        const token = localStorage.getItem('user')
+        const url = `http://127.0.0.1:8000/account/auth/${token}/`
+        fetch(url, option).then(response => response.json()).then(data => this.updateUserForm(data))
+        const address_url = 'http://127.0.0.1:8000/account/address/'
+        fetch(address_url, option).then(response => response.json()).then(data => this.updateAddress(data))
     }
     render() {
         return (
@@ -13,19 +46,35 @@ class Account extends Component {
                     <div className="nav nav-tabs" id="nav-tab" role="tablist">
                         <a className="nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab"
                            aria-controls="nav-home" aria-selected="true">اطلاعات کاربری</a>
-                        <a className="nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab"
-                           aria-controls="nav-profile" aria-selected="false">آدرس</a>
                         <a className="nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab"
                            aria-controls="nav-contact" aria-selected="false">پیشینه سفارشات</a>
                     </div>
                 </nav>
                 <div className="tab-content" id="nav-tabContent">
                     <div className="tab-pane fade show active" id="nav-home" role="tabpanel"
-                         aria-labelledby="nav-home-tab">...
+                         aria-labelledby="nav-home-tab">
+
+                        <form>
+                            <div className="form-group">
+                                <label htmlFor="fist_name">
+                                    نام:
+                                </label>
+                                <input type="text" name="first_name"/>
+                                <label htmlFor="last_name">
+                                    نام خانوادگی:
+                                </label>
+                                <input type="text" name="last_name"/>
+                                {this.state.address && this.state.address.map((address,index)=>{
+                                    return <h1>hi</h1>
+                                })}
+                            </div>
+                            <div className='form-group'>
+
+                            </div>
+                        </form>
+
                     </div>
-                    <div className="tab-pane fade" id="nav-profile" role="tabpanel"
-                         aria-labelledby="nav-profile-tab">...
-                    </div>
+
                     <div className="tab-pane fade" id="nav-contact" role="tabpanel"
                          aria-labelledby="nav-contact-tab">...
                     </div>
@@ -33,6 +82,9 @@ class Account extends Component {
 
             </div>
         );
+    }
+    componentDidMount() {
+        this.getInfo()
     }
 }
 
